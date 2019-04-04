eaff196b9776c1a7bd00743a9f2b8b7599d7cb95
lixiaonan_3278@sina.com <lixiaonan_3278@sina.com>
2019-04-04 15:38:40 +0800

    添加pefer ip

--- src/main/java/com/servicea/servicea/ServiceaApplication.java
+++ src/main/java/com/servicea/servicea/ServiceaApplication.java
@@ -2,9 +2,17 @@ package com.servicea.servicea;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
+@EnableEurekaClient
 @SpringBootApplication
+@EnableFeignClients
 public class ServiceaApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(ServiceaApplication.class, args);
--- src/main/java/com/servicea/servicea/controller/ServiceAController.java
+++ src/main/java/com/servicea/servicea/controller/ServiceAController.java
@@ -3,6 +3,7 @@ package com.servicea.servicea.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @RequestMapping("/")
 public class ServiceAController {
@@ -10,5 +11,12 @@ public class ServiceAController {
     @RequestMapping("/invoke")
     public String invoke(){
         return "servicea";
+
+    }
+
+
+    @RequestMapping("/invokeAdd")
+    public Integer invokeAdd(int a,int b){
+        return a+b;
     }
 }
